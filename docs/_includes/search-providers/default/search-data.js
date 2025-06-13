3b8bb0e5b93bcb64149080c3849f92954e07821c
Taisiia Nekrasova <taisiia.nekrasova@arm.com>
2025-06-13 11:41:01 +0100

    Update search-data.js

--- docs/_includes/search-providers/default/search-data.js
+++ docs/_includes/search-providers/default/search-data.js
@@ -1,15 +1,19 @@
-window.TEXT_SEARCH_DATA={
+<script>
+window.TEXT_SEARCH_DATA = {
   {%- for _collection in site.collections -%}
-    {%- unless forloop.first -%},{%- endunless -%}
-    '{{ _collection.label }}':[
+    '{{ _collection.label }}': [
       {%- for _article in _collection.docs -%}
-      {%- unless forloop.first -%},{%- endunless -%}
-      {'title':{{ _article.title | jsonify }},
-       'content':{{ _article.content | strip_html | strip_newlines | jsonify }},
-       {%- include snippets/prepend-baseurl.html path=_article.url -%}
-       {%- assign _url = __return -%}
-       'url':{{ _url | jsonify }}}
+        {
+          'title': {{ _article.title | jsonify }},
+          'content': {{ (_article.content | default: _article.excerpt | default: _article.description | strip_html | strip_newlines) | jsonify }},
+          {%- include snippets/prepend-baseurl.html path=_article.url -%}
+          {%- assign _url = __return -%}
+          'url': {{ _url | jsonify }}
+        }
+        {%- unless forloop.last -%},{%- endunless -%}
       {%- endfor -%}
     ]
+    {%- unless forloop.last -%},{%- endunless -%}
   {%- endfor -%}
 };
+</script>
